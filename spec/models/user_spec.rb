c1b4be24633e392e1978950b47ad6c964a690758
Anton <steewenson.evan@gmail.com>
2016-01-17 16:41:49 +0500

    two simple tests for check user model and her ability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be successfully created' do # проверим, что юзер успешно создаётся. Если не провалидируется мыло или пароль будет коротким, тест провалится
+    user = create_and_get_user(email: 'example@example.com', password: '12345678', full_name: 'example_user')
+    expect(user.full_name).to eq('example_user')
+  end
+
+  it 'should can manage all' do
+    user = create_and_get_user(email: 'example@example.com', password: '12345678', full_name: 'example_user')
+    user.add_role('admin')  # тест провалится, если роль будет не админ
+    expect(Ability.new(user).can?(:manage, :all)).to eq(true)
+  end
+
+  private
+  def create_and_get_user(option)
+    User.create(option)
+    User.find_by(email: option[:email]) if option[:email].present?
+  end
 end
